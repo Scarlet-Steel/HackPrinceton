c480c43a3790aa5b1b6b0ddf7bed770324370dec
Julian <suikunkun@gmail.com>
2015-06-02 23:16:33 -0400

    emptied. will explain if you ask me...

--- app/background.js
+++ app/background.js
@@ -1,17 +1,1 @@
 
-//listener:
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-	var favorite = localStorage["favorite_colour"];
-	if (!favorite) {
-  	//should probably do something here?
-	}
-        
-        if (request.greeting == "hello"){
-          sendResponse({farewell: favorite});
-        }else{
-          sendResponse({}); // snub them.
-        }
-  });
-
-
