2bd04a86786feb1298f08d16dde0e507af0cc28b
Hill Chang <Hillzc@users.noreply.github.com>
2015-11-14 15:28:37 -0500

    Update findSourcesTest.js

--- findSourcesTest.js
+++ findSourcesTest.js
@@ -1,3 +1,6 @@
-var rawText = ["qqqqq References"]
+var rawText = ["random shit blah References"]
 
-//Search backwards through file until 
+//Search backwards through file until you see "References"
+//Take everything char by char until (****) with 4 integer chars
+//Skip the date and then take evreything until .
+//Add that whole thing using paper.children.push(citation)
